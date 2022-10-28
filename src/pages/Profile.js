568ce2aa3ff898fbd6090cad33265625e5767818
Zoe Johnston <82781352+ZoeJ2020@users.noreply.github.com>
2022-10-28 14:03:07 +0100

    created profile.js

---
+++ src/pages/Profile.js
@@ -0,0 +1,11 @@
+import { useState, useEffect } from "react";
+
+function Profile({userName}) {
+    const [loading, setLoading] = useState(false); 
+    const [profile, setProfile] = useState({}); 
+
+    return ( 
+     );
+}
+
+export default Profile;
